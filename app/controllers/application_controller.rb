f252439605f604472cd27029be5ba13b103f0fcf
jessejennings <jessejenn@gmail.com>
2015-09-07 23:04:12 -0700

    Change indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 
   protected
  
-   def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up) << [:first_name,:last_name, :avatar, :avatar_cache, :role]
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name,:last_name, :avatar, :avatar_cache, :role]
    	devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :avatar, :avatar_cache, :role]
-   end
+  end
 end
